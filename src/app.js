45495b7cae330fc1826309ea4cf9845a5022e30d
samfdev <103870235+samfdev@users.noreply.github.com>
2022-04-20 21:10:51 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
+// Age effect
+const birthDate = new Date("Febuary 04, 2007 07:52:00");
+let lastAge = '';
+setInterval(() => {
+    const age = `${Math.round((new Date().getTime()-birthDate.getTime())/(60 * 60 * 24 * 365.25 / 100000))/100000000}`;
+    for (let i = 0; i < 11; i++) {
+        if (lastAge[i] !== age[i]) {
+            // console.log(age[i]);
+        }
+    }
+    document.querySelector('span#age').innerText = age+'0'.repeat(11-age.length);
+    lastAge = age;
+}, 100)
+
 // Scroll effects
 window.addEventListener('scroll', () => {
     const render = document.querySelector('img.render');
     const name = document.querySelector('h1.name');
     const nav = document.querySelector('nav');
 
-    if (window.scrollY > 0) nav.style.backgroundColor = 'rgba(0 0 0 / 10%)';
+    if (window.scrollY > 0) nav.style.backgroundColor = 'rgba(19 21 27 / 90%)';
     else nav.style.backgroundColor = '';
     
     document.querySelector('header.landing').style.filter = `saturate(${100-window.scrollY/15}%)`;
@@ -15,3 +29,11 @@ window.addEventListener('scroll', () => {
     name.style.transform = `translateY(${window.scrollY*0.1}px)`;
     render.style.transform = `translateY(${window.scrollY*0.3}px)`;
 });
+
+// Pricing
+for (let e of document.querySelectorAll('li.option')) {
+    e.querySelector('input').onclick = () => {
+        e.querySelector('div.check-wrapper').style.backgroundColor = e.querySelector('input').checked?'var(--hl-dark)':'var(--bg-light)';
+    }
+    e.querySelector('input').onclick();
+}
